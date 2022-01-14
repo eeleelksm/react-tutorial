8fe8c06294e8baaf51dad18e26886288adac1499
Keshia-Lee Martin <keshialeemartin@gmail.com>
2022-01-14 12:34:35 -0500

    creating menu animation and menu links

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,9 +2,9 @@ import "./Topbar.scss";
 import Person from "@mui/icons-material/Person";
 import Mail from "@mui/icons-material/Mail";
 
-function Topbar() {
+function Topbar({ menuOpen, setMenuOpen }) {
 	return (
-		<div className="topbar" id="topbar">
+		<div className={"topbar " + (menuOpen && "active")}>
 			<div className="wrapper">
 				<div className="left">
 					<a href="#intro" className="logo">
@@ -19,7 +19,13 @@ function Topbar() {
 						<span>keshialeemartin@gmail.com</span>
 					</div>
 				</div>
-				<div className="right"></div>
+				<div className="right">
+					<div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+						<span className="line1"></span>
+						<span className="line2"></span>
+						<span className="line3"></span>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
